96d2a9c00e4beb167c8e8478825343544228f14f
ainihu <1448122879@qq.com>
2020-04-27 09:20:57 +0800

    剑指offer-第三、四题

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 //#include "leetcode/leetcode_17.hpp"
 // #include "huawei/practice_6.hpp"
 #include <bits/stdc++.h>
-#include "sword2offer/question_02.hpp"
+#include "sword2offer/question_04.hpp"
+#include "gtest/gtest.h"
 
 using namespace std;
 
 
-int main() {
-    test();
-    return 0;
-}
+//int main() {
+//    test();
+//    return 0;
+//}
 
+int main(int argc, char *argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ sword2offer/question_04.hpp
@@ -0,0 +1,23 @@
+/* *** 代码是写给人看，顺带让机器执行 ***
+ * ***  永远不要相信任何传进来的参数 ***
+ * 
+ * 文件: question_04.hpp
+ * 时间: 2020/4/17-10:24
+ * 作者: Pip
+ */
+
+#include <bits/stdc++.h>
+#include "gtest/gtest.h"
+
+using namespace std;
+
+
+string replackBlank(string str) {
+    string ret;
+    return ret;
+}
+
+
+TEST(question_04, replackBlank) {
+    EXPECT_EQ(replackBlank(""), "");
+}
