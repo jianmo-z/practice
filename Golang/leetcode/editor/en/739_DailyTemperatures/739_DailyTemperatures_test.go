57c759f5eaefe787a297c3898bbdc48fcf9d50d3
jianmo-z <jianmo.pip@gmail.com>
2024-12-18 11:20:22 +0800

    leetcode 739

--- Golang/leetcode/editor/en/739_DailyTemperatures/739_DailyTemperatures_test.go
+++ Golang/leetcode/editor/en/739_DailyTemperatures/739_DailyTemperatures_test.go
@@ -39,10 +39,39 @@
  *
  */
 
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
 // @lc code=start
 func dailyTemperatures(temperatures []int) []int {
-	return nil
+
+	res := make([]int, len(temperatures))
+
+	for i := 0; i < len(temperatures); i++ {
+		for j := i + 1; j < len(temperatures); j++ {
+			if temperatures[j] > temperatures[i] {
+				res[i] = j - i
+				break
+			}
+		}
+	}
+
+	return res
 }
 
 // @lc code=end
 
+func TestDailyTemperatures(t *testing.T) {
+	temperatures := []int{73, 74, 75, 71, 69, 72, 76, 73}
+	expect := []int{1, 1, 4, 2, 1, 1, 0, 0}
+	assert.Equal(t, expect, dailyTemperatures(temperatures))
+
+	temperatures = []int{30, 40, 50, 60}
+	expect = []int{1, 1, 1, 0}
+	assert.Equal(t, expect, dailyTemperatures(temperatures))
+}
